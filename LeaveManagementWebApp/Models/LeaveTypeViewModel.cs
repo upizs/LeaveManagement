4c48b1f387bf1125e62dcf3c44922b413103ee1a
Toms Upis <toms.upizs@gmail.com>
2021-01-05 22:04:03 +0000

    Created ViewModels

---
+++ LeaveManagementWebApp/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementWebApp.Models
+{
+    public class DetailsLeaveTypeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
