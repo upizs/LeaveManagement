3cfbb1780ca36222f7256b644cc29a8879d91474
Toms Upis <toms.upizs@gmail.com>
2021-01-05 20:50:15 +0000

    Added Database and EntityFramework functionality

--- LeaveManagementWebApp/Repository/LeaveHistoryRepository.cs
+++ LeaveManagementWebApp/Repository/LeaveHistoryRepository.cs
@@ -17,32 +17,36 @@ namespace LeaveManagementWebApp.Repository
 
         public bool Create(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Add(entity);
+            return Save();
         }
 
         public bool Delete(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Remove(entity);
+            return Save();
         }
 
         public ICollection<LeaveHistory> FindAll()
         {
-            throw new NotImplementedException();
+            return _db.LeaveHistories.ToList();
         }
 
         public LeaveHistory FindById(int id)
         {
-            throw new NotImplementedException();
+            return _db.LeaveHistories.Find(id);
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            var changes = _db.SaveChanges();
+            return changes > 0;
         }
 
         public bool Update(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Update(entity);
+            return Save();
         }
     }
 }
--- LeaveManagementWebApp/Repository/LeaveTypeRepository.cs
+++ LeaveManagementWebApp/Repository/LeaveTypeRepository.cs
@@ -19,22 +19,26 @@ namespace LeaveManagementWebApp.Repository
 
         public bool Create(LeaveType entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveTypes.Add(entity);
+            return Save();
+
         }
 
         public bool Delete(LeaveType entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveTypes.Remove(entity);
+            return Save();
         }
 
         public ICollection<LeaveType> FindAll()
         {
-            throw new NotImplementedException();
+           return _db.LeaveTypes.ToList();
         }
 
         public LeaveType FindById(int id)
         {
-            throw new NotImplementedException();
+            return _db.LeaveTypes.Find(id);
+            
         }
 
         public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
@@ -44,12 +48,16 @@ namespace LeaveManagementWebApp.Repository
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            //if SaveChanges returns less than one change means something has gone
+            //wrong and no changes where saved.
+            var changes =  _db.SaveChanges();
+            return changes > 0;
         }
 
         public bool Update(LeaveType entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveTypes.Update(entity);
+            return Save();
         }
     }
 }
