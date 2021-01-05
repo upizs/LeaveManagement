e7c80bf932a46a0ad1ca56fc996637879184eb27
Toms Upis <toms.upizs@gmail.com>
2021-01-05 08:27:13 +0000

    Initial Commit. With Leave tables and Employee Data points created

---
+++ LeaveManagementWebApp/Data/LeaveType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementWebApp.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+}
---
+++ LeaveManagementWebApp/Data/Migrations/20210105082151_ChangedMaxLengthForNameInLeaveType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeaveManagementWebApp.Data.Migrations
+{
+    public partial class ChangedMaxLengthForNameInLeaveType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "LeaveTypes",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "LeaveTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
